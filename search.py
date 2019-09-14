39730e52ff803234bbedbc270b929fb16118ab2c
camilo <cortiz20@amherst.edu>
2019-09-14 16:41:59 -0400

    NODE class ;-)

--- search.py
+++ search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -87,6 +87,8 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
+    node = Node(None, None, problem.getStartState())
+
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
@@ -111,6 +113,12 @@ def aStarSearch(problem, heuristic=nullHeuristic):
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
 
+
+class Node:
+    def __init__(self, parent, action, state):
+        self.parent = parent
+        self.action = action
+        self.state = state
 
 # Abbreviations
 bfs = breadthFirstSearch
